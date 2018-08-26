95bec22a7ea612cd466d75b5b50eada0ab884b0e
Jason Tiscione <tiscione@gmail.com>
2018-08-26 12:59:31 -0600

    Test now includes both JS and WASM versions

--- test/test.js
+++ test/test.js
@@ -10,76 +10,83 @@ fetch('../wasm/waves.wasm').then(response => response.arrayBuffer())
     });
   })
   .then((result) => {
-    let output = '';
-    const instance = result.instance;
+
     const width = 5;
     const height = 5;
-    const algorithm = instance.exports;
-    algorithm.init(width, height);
-    const offset = algorithm.getStartByteOffset();
-    console.log('offset', offset);
-    const unsignedMemory = new Uint32Array(instance.exports.memory.buffer, offset, 6 * width * height);
-    const signedMemory = new Int32Array(instance.exports.memory.buffer, offset, 6 * width * height);
+
+    const jsAlgorithm = jsWaveAlgorithm(width, height);
+    const wasmAlgorithm = wasmWaveAlgorithm(result, width, height);
 
     const outerDiv = document.getElementById('outer');
 
-    // Set initial conditions: designate outer boundary as a wall
-    for (let i=0; i < 5; i++) {
-      signedMemory[125 + i] = 1;
-      signedMemory[145 + i] = 1;
-      signedMemory[125 + (5 * i)] = 1;
-      signedMemory[129 + (5 * i)] = 1;
-    }
-    // Plant large and small values in top left and right corners
-    signedMemory[31] = 0x3FFFFFFF;
-    signedMemory[43] = -0x40000000;
+    [jsAlgorithm, wasmAlgorithm].forEach((algorithm, algorithmIndex) => {
+      const algorithmDiv = document.createElement('div');
+      algorithmDiv.className = 'algorithm';
 
-    for (let run = 0; run < 8; run++) {
-      const columnDiv = document.createElement('div');
-      columnDiv.className = 'column';
-      outerDiv.appendChild(columnDiv);
+      const unsignedMemory = algorithm.getEntireArray();
+      const signedMemory = new Int32Array(unsignedMemory.buffer, unsignedMemory.byteOffset);
 
-      const pixelsDiv = document.createElement('div');
-      pixelsDiv.className = 'pixels';
-      columnDiv.appendChild(pixelsDiv);
-
-      const outputDiv = document.createElement('div');
-      columnDiv.appendChild(outputDiv);
-
-      algorithm.singleFrame(0, 0);
-
-      for (let i = 0; i < 25; i++) {
-        const pixelDiv = document.createElement('div');
-        const rgba = unsignedMemory[i];
-        const red = rgba & 0xFF;
-        const green = (rgba >> 8) & 0xFF;
-        const blue = (rgba >> 16) & 0xFF;
-        const alpha = ((rgba >> 24) & 0xFF) / 255;
-        pixelDiv.style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
-        pixelsDiv.appendChild(pixelDiv);
+      // Set initial conditions: designate outer boundary as a wall
+      for (let i = 0; i < 5; i++) {
+        signedMemory[125 + i] = 1;
+        signedMemory[145 + i] = 1;
+        signedMemory[125 + (5 * i)] = 1;
+        signedMemory[129 + (5 * i)] = 1;
       }
-      let output = ('<br>RGBA:<br>');
-      for (let i = 0; i < 24; i++) {
-        output += (`${i}\t${unsignedMemory[i].toString(16)}<br>`);
+
+      // Plant large and small values in top left and bottom right corners of center 3x3 region
+      signedMemory[31] = 0x3FFFFFFF;
+      signedMemory[43] = -0x40000000;
+
+      for (let run = 0; run < 10; run++) {
+        const columnDiv = document.createElement('div');
+        columnDiv.className = 'column';
+        algorithmDiv.appendChild(columnDiv);
+
+        const pixelsDiv = document.createElement('div');
+        pixelsDiv.className = 'pixels';
+        columnDiv.appendChild(pixelsDiv);
+
+        const outputDiv = document.createElement('div');
+        columnDiv.appendChild(outputDiv);
+
+        algorithm.singleFrame(0, 0);
+
+        for (let i = 0; i < 25; i++) {
+          const pixelDiv = document.createElement('div');
+          const rgba = unsignedMemory[i];
+          const red = rgba & 0xFF;
+          const green = (rgba >> 8) & 0xFF;
+          const blue = (rgba >> 16) & 0xFF;
+          const alpha = ((rgba >> 24) & 0xFF) / 255;
+          pixelDiv.style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
+          pixelsDiv.appendChild(pixelDiv);
+        }
+        let output = (run === 0 ? ['<b>JAVASCRIPT</b>', '<b>WEBASSEMBLY</b>'][algorithmIndex] : '&nbsp;');
+        output += ('<br>RGBA<br>');
+        for (let i = 0; i < 24; i++) {
+          output += (`${i}\t${unsignedMemory[i].toString(16)}<br>`);
+        }
+        for (let i = 25; i < 150; i++) {
+          if (i === 25) {
+            output += '<br>u:<br>';
+          }
+          if (i === 50) {
+            output += '<br>swap:<br>';
+          }
+          if (i === 75) {
+            output += '<br>v:<br>';
+          }
+          if (i === 100) {
+            output += '<br>force:<br>';
+          }
+          if (i === 125) {
+            output += '<br>status:<br>';
+          }
+          output += (`${i}\t${signedMemory[i]}<br>`);
+        }
+        outputDiv.innerHTML = output;
       }
-      for (let i = 25; i < 150; i++) {
-        if (i === 25) {
-          output += '<br>u:<br>';
-        }
-        if (i === 50) {
-          output += '<br>swap:<br>';
-        }
-        if (i === 75) {
-          output += '<br>v:<br>';
-        }
-        if (i === 100) {
-          output += '<br>force:<br>';
-        }
-        if (i === 125) {
-          output += '<br>status:<br>';
-        }
-        output += (`${i}\t${signedMemory[i]}<br>`);
-      }
-      outputDiv.innerHTML = output;
-    }
+      outerDiv.appendChild(algorithmDiv);
+    });
   });
